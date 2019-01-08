44ca90624450c840877a1f1c5cf2c8ad55b92544
Asmund <smund3@gmail.com>
2019-01-08 03:15:59 +0100

    remove unused home route

--- routes/main-route.php
+++ routes/main-route.php
@@ -5,15 +5,8 @@ use \Rapd\Router\Route;
 use \Rapd\View;
 
 Router::add(new Route(
-	"home",
-	"/",
-	function(){
-		return View::render("home");
-	}
-));
-Router::add(new Route(
 	"browse_by_item",
-	"/browse/items",
+	"/",
 	function(){
 		return View::render("browse_by_item");
 	}
